cdf7809a07b01abb1edcc4b63fdb510c19170a9d
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-11-30 00:52:31 +0100

    Setting up the skybox, so it's pretty

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,40 @@
 #include <glm/glm.hpp>
 
 #include "lib/ImGuiInit.h"
+#include "lib/Shader.h"
 
 #include <iostream>
 
 const unsigned int SCR_WIDTH = 1400;
 const unsigned int SCR_HEIGHT = 1000;
+
+float skyboxVertices[] =
+{
+        -1.0f, -1.0f,  1.0f,    // 0
+         1.0f, -1.0f,  1.0f,    // 1
+         1.0f, -1.0f, -1.0f,    // 2
+        -1.0f, -1.0f, -1.0f,    // 3
+        -1.0f,  1.0f,  1.0f,    // 4
+         1.0f,  1.0f,  1.0f,    // 5
+         1.0f,  1.0f, -1.0f,    // 6
+        -1.0f,  1.0f, -1.0f     // 7
+};
+
+unsigned int skyboxIndices[] =
+{
+        0, 1, 2, // Bottom
+        0, 2, 3,
+        4, 5, 6, // Top
+        4, 6, 7,
+        0, 1, 5, // Front
+        0, 5, 4,
+        2, 3, 7, // Back
+        2, 7, 6,
+        1, 2, 6, // Right
+        1, 6, 5,
+        0, 3, 7, // Left
+        0, 7, 4
+};
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -34,8 +63,12 @@ int main() {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1; // Terminate program
     }
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT); // Set viewport size (in pixels)
     // Configure global OpenGL state
     glEnable(GL_DEPTH_TEST); // Enable depth testing
+
+    // Shaders
+    Shader shaderProgram();
 
     // Initialize ImGui
     init_imgui(window);
