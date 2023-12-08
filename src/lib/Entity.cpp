eb0ee7f84f191da9f24ca5322509872ab147cbcb
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-12-08 04:43:14 +0100

    Changed Entity Class

--- src/lib/Entity.cpp
+++ src/lib/Entity.cpp
@@ -1,39 +1,113 @@
 #include "Entity.h"
 
-Entity::Entity(Entity *parent) {
-    parent->addChild(this);
+Entity::Entity() = default;
+
+Entity::Entity(const glm::mat4 &model) : modelMatrix(model) {
+    glm::quat rotation;
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(model, scale, rotation, position, skew, perspective);
+    eulerRotation = glm::eulerAngles(rotation);
+}
+
+void Entity::update(bool parentDirty) {
+    parentDirty |= dirty;
+
+    if (parentDirty) {
+        computeModelMatrix();
+    }
+
+    for (auto child : children) {
+        child->update(parentDirty);
+    }
+}
+
+void Entity::computeModelMatrix() {
+    const glm::mat4 transformX = glm::rotate(glm::mat4(1.0f), eulerRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+    const glm::mat4 transformY = glm::rotate(glm::mat4(1.0f), eulerRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+    const glm::mat4 transformZ = glm::rotate(glm::mat4(1.0f), eulerRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    const glm::mat4 rotationMatrix = transformX * transformY * transformZ;
+
+    modelMatrix = glm::translate(glm::mat4(1.0f), position) * rotationMatrix * glm::scale(glm::mat4(1.0f), scale);
+
+    if (parent != nullptr) {
+        modelMatrix = parent->getModelMatrix() * modelMatrix;
+    }
+
+    dirty = false;
+}
+
+void Entity::computeModelMatrix(const glm::mat4 &parentGlobalMatrix) {
+    computeModelMatrix();
+    modelMatrix = parentGlobalMatrix * modelMatrix;
+}
+
+void Entity::setParent(Entity *parent) {
+    this -> parent = parent;
+    parent -> addChild(this);
 }
 
 void Entity::addChild(Entity *child) {
     children.emplace_back(child);
-    children.back()->parent = this;
 }
 
-void Entity::draw() {
-    if (model != nullptr) {
-        model->draw(*shader, transform);
-    }
+void Entity::setLocalRotation(const glm::vec3 &rotation) {
+    eulerRotation = rotation;
+    dirty = true;
 }
 
-void Entity::updateSelfAndChild() {
-    if (transform.isDirty()) {
-        forceUpdateSelfAndChild();
-        return;
-    }
-
-    for (auto&& child : children) {
-        child->updateSelfAndChild();
-    }
+void Entity::setLocalRotationX(float rotation) {
+    eulerRotation.x = rotation;
+    dirty = true;
 }
 
-void Entity::forceUpdateSelfAndChild() {
-    if (parent) {
-        transform.computeModelMatrix(parent->transform.getModelMatrix());
-    } else {
-        transform.computeModelMatrix();
-    }
+void Entity::setLocalRotationY(float rotation) {
+    eulerRotation.y = rotation;
+    dirty = true;
+}
 
-    for (auto&& child : children) {
-        child->forceUpdateSelfAndChild();
-    }
+void Entity::setLocalRotationZ(float rotation) {
+    eulerRotation.z = rotation;
+    dirty = true;
+}
+
+void Entity::setLocalPosition(const glm::vec3 &position) {
+    this -> position = position;
+    dirty = true;
+}
+
+void Entity::setLocalScale(const glm::vec3 &scale) {
+    this -> scale = scale;
+    dirty = true;
+}
+
+void Entity::setModelMatrix(const glm::mat4 &model) {
+    modelMatrix = model;
+    glm::quat rotation;
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(model, scale, rotation, position, skew, perspective);
+    eulerRotation = glm::eulerAngles(rotation);
+    dirty = true;
+}
+
+const glm::vec3 &Entity::getLocalRotation() const {
+    return eulerRotation;
+}
+
+const glm::vec3 &Entity::getLocalPosition() const {
+    return position;
+}
+
+const glm::vec3 &Entity::getLocalScale() const {
+    return scale;
+}
+
+const glm::mat4 &Entity::getModelMatrix() const {
+    return modelMatrix;
+}
+
+bool Entity::isDirty() const {
+    return dirty;
 }
--- src/lib/Entity.h
+++ src/lib/Entity.h
@@ -1,36 +1,50 @@
 #ifndef GP_3_ENTITY_H
 #define GP_3_ENTITY_H
 
+#include "glm/trigonometric.hpp"
+#include "glm/gtc//matrix_transform.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
+
 #include "Model.h"
 #include "Transform.h"
 
-#include <list>
-
 class Entity {
 public:
-    // Scene graph
-    std::list<Entity*> children;
-    Entity* parent = nullptr;
+    Entity();
+    Entity(const glm::mat4& model);
 
-    // Space information
-    Shader *shader = nullptr;
-    Model *model = nullptr;
-    Transform transform;
+    void update(bool parentDirty = false);
+    void computeModelMatrix();
+    void computeModelMatrix(const glm::mat4& parentGlobalMatrix);
 
-    glm::mat4* matrix;
+    void setParent(Entity* parent);
+    void addChild(Entity* child);
 
-    // Constructor
-    Entity() {};
-    Entity(Entity *parent);
+    void setLocalRotation(const glm::vec3& rotation);
+    void setLocalRotationX(float rotation);
+    void setLocalRotationY(float rotation);
+    void setLocalRotationZ(float rotation);
+    void setLocalPosition(const glm::vec3& position);
+    void setLocalScale(const glm::vec3& scale);
+    void setModelMatrix(const glm::mat4& model);
 
-    // Add a child to the scene graph
-    void addChild(Entity *child);
-    // Draw the entity
-    void draw();
-    // Update transform if it was changed
-    void updateSelfAndChild();
-    // Force update transform
-    void forceUpdateSelfAndChild();
+    [[nodiscard]] const glm::vec3& getLocalRotation() const;
+    [[nodiscard]] const glm::vec3& getLocalPosition() const;
+    [[nodiscard]] const glm::vec3& getLocalScale() const;
+    [[nodiscard]] const glm::mat4& getModelMatrix() const;
+
+    [[nodiscard]] bool isDirty() const;
+private:
+    unsigned int instanceVBO = 0;
+
+    glm::vec3 position = glm::vec3(0.0f);
+    glm::vec3 eulerRotation = glm::vec3(0.0f);
+    glm::vec3 scale = glm::vec3(1.0f);
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
+
+    bool dirty = true;
+    Entity *parent = nullptr;
+    std::vector<Entity*> children;
 };
 
 
